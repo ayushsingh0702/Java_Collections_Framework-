0cd3cba15df2ff79f072fec6ccd0bbbc8d73cc54
Ayush Singh <138690808+ayushsingh0702@users.noreply.github.com>
2025-09-24 17:47:23 +0530

    THIS IS THE FIRST SET CODE

---
+++ COLLECTION_FRAMWORK/src/Main.java
@@ -0,0 +1,98 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+//public class Main {
+//    public static void main(String[] args) {
+//        ArrayList<String> array =new ArrayList<>();
+//        array.add("ayush");
+//        array.add("aman");
+//        System.out.print(array);
+//        System.out.println();
+
+        // second version
+//        ArrayList<Integer> array2 = new ArrayList<>();
+//        array2.add(1);
+//        array2.add(2);
+//        array2.add(3);
+//        array2.add(4);
+//        array2.add(2,20);
+//        System.out.print(array2);
+
+////     removing function
+//        array2.remove(1);
+//        System.out.println(array2);
+//        //vvip
+//        array2.remove(Integer.valueOf(20));
+//        System.out.println(array2);
+//
+//        array2.clear();
+//        System.out.println(array2);
+////set hai ye
+//        array2.set(2,2000);
+//        System.out.println(array2);
+
+//        ArrayList<Integer> newList=new ArrayList<>();
+//        newList.add(10);
+//        newList.add(20);
+//        //version 3
+//        array2.addAll(newList);
+//        System.out.println();
+//        System.out.print(array2);
+//        System.out.println();
+//        System.out.println(array2.get(1));
+
+//        //itration
+//
+//   for(int i=0;i<array2.size();i++){
+//       System.out.println(array2.get(i));
+//   }
+//
+//   for(Integer element:array2){
+//       System.out.println(element);
+//   }
+//        Iterator<Integer> it = array2.iterator();
+//   while(it.hasNext()){
+//       System.out.println(it.next());
+//   }
+
+
+////// stack
+//        Stack<String> arr=new Stack<>();
+//        arr.push("ayush");
+//        arr.push("aman");
+//        arr.push("rishi");
+//        arr.push("pratyush");
+//        arr.push("babu");
+//        System.out.println(arr);
+//        System.out.println(arr.peek());
+//        System.out.println(arr.pop());
+//        System.out.println(arr);
+//        System.out.println(arr.peek());
+//
+
+////queue
+        //offer
+        //poll
+        //peek
+        // Queue<Integer>arr=new LinkedList<>();
+//       LinkedList<Integer>arr=new LinkedList<>();
+//        arr.offer(10);
+//        arr.offer(20);
+//        arr.offer(30);
+//        System.out.println(arr);
+//        System.out.println(arr.poll());
+//        System.out.println(arr.peek());
+//        System.out.println(arr.poll());
+
+
+
+
+//    }
+//}
